1fc1dc12ca3db6960a7c3a6ad2ebb1b49fc83b94
Alan Lozano <lozanomurilloam@gmail.com>
2020-11-18 17:22:44 -0600

    Nice

--- src/router/router.js
+++ src/router/router.js
@@ -30,16 +30,31 @@ var noticias = [
     
 ];
 
+var bolsa = [
+    {
+        imagen : 'https://altaglatam.com/wp-content/uploads/2015/03/COCA-COLA-LOGO.jpg',
+        titulo: 'Practicante RH',
+        descripcion: 'Se solicita a un practicante del area de recursos humanos, apoyo economico a tratar, de preferencia estudiante cursando su septimo cuatrimestre.',
+        link: '#'
+    },
+    {
+        imagen : 'https://upload.wikimedia.org/wikipedia/commons/0/0f/Pepsi_logo_2014.svg',
+        titulo: 'Diseñador Grafico',
+        descripcion: 'Se solicita practicante del area de diseño grafico, apoyo economico fijo, solo estudiantes de sexto cuatrimestre en adelante',
+        link: '#'
+    },
+]
+
 router.get('/',(req,res)=>{
     res.render('index',{noticias});
 });
 
-router.get('/egresados/:valor*?',(req,res)=>{
-    if(req.params.valor == 1){
-        res.render('egresados',{pagina : 1});
-    } else {
-        res.render('egresados',{pagina : 2});
-    }
+router.get('/bolsaTrabajo',(req,res)=>{
+    res.render('bolsaTrabajo',{bolsa});
+});
+
+router.get('/cuerposac', (req,res)=> {
+    res.render('cuerposAcademicos');
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const engine = require('ejs-mate');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -14,7 +15,11 @@ app.set('port',process.env.PORT||5000);
 app.use(express.static(__dirname+'/public'));
 
 //Middlewares
+const uri = 'mongodb+srv://admin:1234@clusteruno.ikprp.mongodb.net/utng?retryWrites=true&w=majority'
 app.use(morgan('dev'));
+mongoose.createConnection(uri, {useUnifiedTopology : true, useNewUrlParser: true})
+    .then(() => console.log('Conexión exitosa'))
+    .catch(err => console.log(err));
 
 //Rutas
 app.use(require('./router/router'));
